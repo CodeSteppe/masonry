fcea81777cf0e672378d79276fb05b3e4593a8f1
bruce_zhang <bruce_zhang@trendmicro.com>
2022-06-17 15:46:24 +0800

    update

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const masonry = document.querySelector('.masonry');
 
+let cardCount = 0;
+const colHeights = [0, 0, 0, 0];
+
 async function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
@@ -15,38 +18,32 @@ async function loadImage(src, element) {
 async function createCard() {
   const card = document.createElement('div');
   card.classList.add('card');
-  const randomH = Math.round(Math.random() * 800) + 200;
+  card.style.position = 'fixed';
+  card.style.visibility = 'hidden';
+  const randomH = Math.round(Math.random() * 500) + 300;
   const src = `http://source.unsplash.com/random/400x${randomH}`;
   const img = await loadImage(src, document.createElement('img'));
   if (img) {
     card.append(img);
+    document.body.appendChild(card);
+    await sleep(500);
+    const cardIndex = cardCount;
+    const colIndex = (cardIndex + 1) % 4;
+    colHeights[colIndex] += card.clientHeight;
+    console.log('add card', card, cardIndex, card.clientHeight, `col ${colIndex} height`, colHeights[colIndex]);
+    masonry.style.height = Math.max(...colHeights) + 10 + 'px';
     const divider1 = masonry.querySelector('.divider1');
     masonry.insertBefore(card, divider1);
-    await sleep(500);
-    const cardIndex = masonry.querySelectorAll('.card').length - 1;
-    const cardBottom = card.offsetTop + card.offsetHeight;
-    return {
-      card,
-      cardIndex,
-      cardBottom
-    }
+    card.style.position = '';
+    card.style.visibility = '';
+    cardCount++;
   }
 }
 
 function loadData() {
-  const tasks = [];
   for (let i = 0; i < 12; i++) {
-    tasks.push(createCard());
+    (createCard());
   }
-  // const currentHeight = masonry.clientHeight;
-  // masonry.style.height = (currentHeight + 3000) + 'px';
-  Promise.all(tasks)
-    .then((result) => {
-      const bottoms = result.map(r => r.cardBottom);
-      const maxBottom = Math.max(...bottoms);
-      console.log('all card rendered', result);
-      masonry.style.height = Math.ceil(maxBottom) + 30 + 'px';
-    });
 }
 
 loadData();
